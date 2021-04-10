cd03e52034a70322631b93c846cb9f404435b354
rpiCrespo <jp.crespo.vargas@gmail.com>
2021-04-10 17:38:32 -0400

    fix bugs Rpi

--- Comparaciones/test.cpp
+++ Comparaciones/test.cpp
@@ -5,82 +5,94 @@
 
 
 
+int N[]={2,20,30,50,100,200,500,1000,1200,1500};
 
-int N[]={2,20,30,50,100,200,500,1000,1200,1500,2000};
+int N_rep=10;
+double temp[10];
+
+struct timespec begin, end;
+
+FILE *fp;
+
 
 
 int **matrizA;  //declaramos un puntero de punteros
 int **matrizB;
 int **matrizC;
 
+float promedio;
+float varianza;
+
+
+
+
 int main()
 {
-matrizA = (int **)malloc (N[0]*sizeof(int *));
-for (int i=0;i<N[0];i++)
+
+
+fp = fopen ( "tiemposmatmult.csv", "w" );
+fprintf(fp,"# N elementos procesados,Tiempo Promedio[ms],STD Tiempo [ms] \n");
+
+
+for(int r=0;r<10;r++)
 {
-matrizA[i] = (int *) malloc (N[0]*sizeof(int));
+
+matrizA = (int **)malloc (N[r]*sizeof(int *));
+for (int i=0;i<N[r];i++)
+{
+matrizA[i] = (int *) malloc (N[r]*sizeof(int));
 }
 
 
-matrizB = (int **)malloc (N[0]*sizeof(int *));
-for (int i=0;i<N[0];i++)
+matrizB = (int **)malloc (N[r]*sizeof(int *));
+for (int i=0;i<N[r];i++)
 {
-matrizB[i] = (int *) malloc (N[0]*sizeof(int));
+matrizB[i] = (int *) malloc (N[r]*sizeof(int));
 }
 
-matrizC = (int **)malloc (N[0]*sizeof(int *));
-for (int i=0;i<N[0];i++)
+matrizC = (int **)malloc (N[r]*sizeof(int *));
+for (int i=0;i<N[r];i++)
 {
-matrizC[i] = (int *) malloc (N[0]*sizeof(int));
+matrizC[i] = (int *) malloc (N[r]*sizeof(int));
 }
-
-
 
 
 
 srand(time(NULL));
 
-for(int i=0;i<N[0];i++){
-for(int j=0;j<N[0];j++){
+for(int i=0;i<N[r];i++){
+for(int j=0;j<N[r];j++){
 matrizA[i][j]= rand()%100;
 matrizB[i][j]=rand()%100;
 }
 }
 
-for(int i=0;i<N[0];i++)
+
+for(int y=0;y<N_rep;y++)
 {
-for(int j=0;j<N[0];j++)
+
+clock_gettime(CLOCK_REALTIME, &begin);
+
+
+for(int i=0;i<N[r];i++)
 {
-for(int g=0;g<N[0];g++)
+for(int j=0;j<N[r];j++)
+{
+for(int g=0;g<N[r];g++)
 {
 matrizC[i][j]+=matrizA[i][g]*matrizB[g][j];
 }
 }
 }
-printf("\n MATRIZ A \n");
-for (int i=0;i<N[0];i++){
-for (int j=0;j<N[0];j++){
-printf("%d ",matrizA[i][j]);
-	}
-printf("\n");
-}
 
-printf("\n MATRIZ B \n");
-
-for (int i=0;i<N[0];i++){
-for (int j=0;j<N[0];j++){
-printf("%d ",matrizB[i][j]);
-        }
-printf("\n");
-}
+clock_gettime(CLOCK_REALTIME, &end);
 
 
-printf("\n MATRIZ C \n");
-for (int i=0;i<N[0];i++){
-for (int j=0;j<N[0];j++){
-printf("%d ",matrizC[i][j]);
-        }
-printf("\n");
+long seconds = end.tv_sec - begin.tv_sec;
+long nanoseconds = end.tv_nsec - begin.tv_nsec;
+double elapsed = 1000*seconds + nanoseconds*1e-6;
+
+temp[y]=elapsed;
 }
 
 
@@ -88,4 +100,29 @@ free(matrizA);
 free(matrizB);
 free(matrizC);
 
+
+
+
+float secs=0;
+
+for(int j=0;j<N_rep;j++)
+{
+secs+=temp[j];
+}
+
+promedio=secs/N_rep; // Calcula el tiempo promedio.
+
+secs=0;
+for(int j=0;j<N_rep;j++)
+{
+secs+=pow(temp[j]-promedio,2);
+}
+
+varianza = sqrt(secs/(N_rep-1));   //calcula la varianza
+
+fprintf(fp,"%d,%.5f,%.5f\n",N[r],promedio,varianza);
+
+}
+fclose(fp);
+return 0;
 }
